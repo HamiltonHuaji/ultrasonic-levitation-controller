52625136fb1668ee08d35bc58769107f06745337
raspberrypi <pi@raspberrypi>
2020-08-15 19:49:12 +0100

    pin map

--- controller.cpp
+++ controller.cpp
@@ -1,25 +1,29 @@
 #include <algorithm>
 #include <atomic>
+#include <array>
 #include <functional>
-#include <string>
 #include <memory>
 #include <mutex>
-#include <pybind11/pybind11.h>
+#include <condition_variable>
 #include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <stdexcept>
+#include <string>
 #include <thread>
 #include <wiringPi.h>
 #define sync delayMicroseconds(5)
 #define foreach(e) for (unsigned char e = GPIOController::pin_min; e <= GPIOController::pin_max; e++)
 struct GPIOController {
-private:
+  private:
     constexpr static unsigned int pin_reset = 27;
     constexpr static unsigned int pin_clock = 28;
     constexpr static unsigned int pin_load  = 29;
     constexpr static unsigned int pin_min   = 0;
     constexpr static unsigned int pin_max   = 17;
     constexpr static unsigned char mask[4]  = {0b0001, 0b0010, 0b0100, 0b1000};
+
+    constexpr static std::array<unsigned char, pin_max - pin_min + 1> pin_map = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 21};
     unsigned char phase_A[pin_max - pin_min + 1];
     unsigned char phase_B[pin_max - pin_min + 1];
     unsigned char *phase_read{phase_A};
@@ -39,11 +43,11 @@ private:
     GPIOController(const GPIOController &c) = delete;
     GPIOController(GPIOController &&c)      = delete;
 
-public:
+  public:
     void set(unsigned, int);
     int get(unsigned);
     void swap_buffer();
-    void fill_buffer(const pybind11::array_t<int>& input);
+    void fill_buffer(const pybind11::array_t<int> &input);
     GPIOController();
     ~GPIOController();
 };
@@ -68,19 +72,19 @@ void GPIOController::send_clock() {
 void GPIOController::send_array() {
     for (unsigned char i = 0; i < 4; i++) {
         foreach (pin) {
-            digitalWrite(pin, ((phase_read[pin] & mask[i])) == 0 ? LOW : HIGH);
+            digitalWrite(pin_map[pin], ((phase_read[pin] & mask[i])) == 0 ? LOW : HIGH);
         }
         send_clock();
     }
 }
 void GPIOController::set_low() {
     foreach (pin) {
-        digitalWrite(pin, LOW);
+        digitalWrite(pin_map[pin], LOW);
     }
 }
 void GPIOController::set_high() {
     foreach (pin) {
-        digitalWrite(pin, HIGH);
+        digitalWrite(pin_map[pin], HIGH);
     }
 }
 void GPIOController::flush() {
@@ -114,20 +118,20 @@ void GPIOController::swap_buffer() {
     std::swap(phase_read, phase_write);
     lock.unlock();
 }
-void GPIOController::fill_buffer(const pybind11::array_t<int>& input){
+void GPIOController::fill_buffer(const pybind11::array_t<int> &input) {
     pybind11::buffer_info bufi = input.request();
-    if(bufi.ndim!=1){throw std::runtime_error("Not a 1-d array");}
-    if(bufi.shape[0]!=GPIOController::pin_max - GPIOController::pin_min + 1){
+    if (bufi.ndim != 1) { throw std::runtime_error("Not a 1-d array"); }
+    if (bufi.shape[0] != GPIOController::pin_max - GPIOController::pin_min + 1) {
         throw std::runtime_error("Size not fit");
     }
-    foreach(pin){
+    foreach (pin) {
         phase_write[pin] = static_cast<int *>(bufi.ptr)[pin];
     }
 }
 GPIOController::GPIOController() {
     wiringPiSetup();
     foreach (pin) {
-        pinMode(pin, OUTPUT);
+        pinMode(pin_map[pin], OUTPUT);
         phase_read[pin]  = 0;
         phase_write[pin] = 0;
     }
